e44fd779dea0531cf0e97f7fc32de7f522a77cb4
IvanPyzhyanov <piv85@mail.ru>
2022-03-27 20:35:48 +0300

    Updated "User" table

--- project/dao/models/user.py
+++ project/dao/models/user.py
@@ -1,21 +1,22 @@
 from project.setup_db import db
 
-users_movies = db.Table('users_movies',
+favorite = db.Table('favorites',
     db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
     db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True)
 )
 
 class User(db.Model):
-    __tablename__ = "users"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    name = db.Column(db.String(255))
-    surname = db.Column(db.String(255))
-    genre = db.relationship("Genre")
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(50), nullable=True)
+    surname = db.Column(db.String(50), nullable=True)
+    favourite_genre = db.Column(db.Integer, db.ForeignKey('genres.id'), nullable=True)
+    genre = db.relationship('Genre', back_populates='users')
     favorites = db.relationship(
-        "Movie",
-        secondary=users_movies,
+        'Movie',
+        secondary=favorite,
         lazy='subquery',
         backref=db.backref('movies', lazy=True),
     )
