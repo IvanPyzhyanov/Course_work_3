539f10a30d3c7019436e932492e0dbc134813bbe
IvanPyzhyanov <piv85@mail.ru>
2022-03-28 20:58:56 +0300

    Updated "Genre" table

--- project/dao/models/genre.py
+++ project/dao/models/genre.py
@@ -6,6 +6,7 @@ class Genre(BaseMixin, db.Model):
     __tablename__ = "genres"
 
     name = db.Column(db.String(100), unique=True, nullable=False)
+    users = db.relationship('User', back_populates='genre')
 
     def __repr__(self):
         return f"<Genre '{self.name.title()}'>"
